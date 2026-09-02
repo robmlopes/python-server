45bd2cb3f426652ade98a7437f9560794512f72c
Robson Martins Lopes <robmlopes@yahoo.com.br>
2026-09-02 02:28:56 +0000

    página principal

---
+++ app.py
@@ -0,0 +1,35 @@
+import http.server
+import socketserver
+
+# DEFINA AQUI A SUA NOVA PORTA
+PORTA = 8080
+
+HTML = """
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Servidor Python</title>
+    <style>
+        body { font-family: sans-serif; text-align: center; margin-top: 50px; }
+        h1 { color: #2c3e50; }
+    </style>
+</head>
+<body>
+    <h1>Servidor Python Ativo!</h1>
+    <p>Esta pagina esta rodando no arquivo app.py.</p>
+</body>
+</html>
+"""
+
+class MeuHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(HTML.encode('utf-8'))
+
+with socketserver.TCPServer(("", PORTA), MeuHandler) as httpd:
+    print(f"Servidor rodando em: http://192.168.68.244:{PORTA}")
+    print("Para desligar, pressione Ctrl+C")
+    httpd.serve_forever()
